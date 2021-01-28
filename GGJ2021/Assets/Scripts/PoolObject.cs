767c8a7d9404c8f8e85fe8bffe397110d78c23c7
OnlyR0ck <guitarilu2019@gmail.com>
2021-01-28 16:46:41 +0300

    Added object pooling and able to click to the ore

---
+++ GGJ2021/Assets/Scripts/PoolObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Pool/PoolObject")]
+public class PoolObject : MonoBehaviour {
+
+    #region Interface
+    public void ReturnToPool () {
+        gameObject.SetActive (false);
+    }
+    #endregion
+}
