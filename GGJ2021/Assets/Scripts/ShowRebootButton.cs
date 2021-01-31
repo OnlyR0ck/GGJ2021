3312798146efc6408723b0a7a874d190668c5478
OnlyR0ck <guitarilu2019@gmail.com>
2021-01-31 16:47:12 +0300

    Reboot

--- GGJ2021/Assets/Scripts/ShowRebootButton.cs
+++ GGJ2021/Assets/Scripts/ShowRebootButton.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ShowRebootButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject _rebootShop;
+    private RectTransform _rectTransform;
+
+    private float speed;
+
     void Start()
     {
-        
+        _rectTransform = _rebootShop.GetComponent<RectTransform>();
+    }
+    // Start is called before the first frame update
+    public void OpenRebootShop()
+    {
+        _rebootShop.SetActive(true);
+        StartCoroutine(MoveForward());
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseRebootShop()
     {
-        
+        _rebootShop.SetActive(false);
+    }
+
+    private IEnumerator MoveForward()
+    {
+        while (_rebootShop.transform.position.x < 6.5f)
+        {
+            yield return new WaitForFixedUpdate();
+            _rectTransform.position = new Vector2(_rectTransform.position.x + 0.02f,
+                _rectTransform.position.y);
+        }
     }
 }
